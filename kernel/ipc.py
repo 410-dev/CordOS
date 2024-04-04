c5799ca928029b29985c1c5e30fe3c7028ace4b8
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-04-05 02:25:26 +0900

    IPC now supports full object on memory

--- kernel/ipc.py
+++ kernel/ipc.py
@@ -5,6 +5,21 @@ import json
 import kernel.partitionmgr as Partitions
 import kernel.clock as Clock
 
+class IPCMemory:
+    memory = {}
+
+    def add(id: str, value: object):
+        IPCMemory.memory[id] = value
+
+    def get(id: str) -> object:
+        return IPCMemory.memory[id]
+
+    def exists(id: str) -> bool:
+        return id in IPCMemory.memory
+
+    def delete(id: str):
+        del IPCMemory.memory[id]
+
 def set(id: str, value: str):
     parentPath: str = f"{Partitions.cache()}/ipc/{id}"
 
@@ -15,6 +30,13 @@ def set(id: str, value: str):
     # Write value to file
     with open(f"{parentPath}/value", 'w') as f:
         f.write(value)
+
+
+
+def setObj(id: str, value: object):
+    # Use static memory
+    IPCMemory.add(id, value)
+
 
 def setJson(id: str, value: dict):
     parentPath: str = f"{Partitions.cache()}/ipc/{id}"
@@ -40,6 +62,10 @@ def delete(id: str):
     # Delete parent path
     os.rmdir(parentPath)
 
+def deleteObj(id: str):
+    # Use static memory
+    IPCMemory.delete(id)
+
 def read(id: str) -> str:
     parentPath: str = f"{Partitions.cache()}/ipc/{id}"
 
@@ -61,12 +87,20 @@ def readJson(id: str) -> dict:
     # Read value from file
     with open(f"{parentPath}/value", 'r') as f:
         return json.loads(f.read())
+
+def readObj(id: str) -> object:
+    # Use static memory
+    return IPCMemory.get(id)
     
 def exists(id: str) -> bool:
     parentPath: str = f"{Partitions.cache()}/ipc/{id}"
 
     # Check if parent path exists
     return os.path.exists(parentPath)
+
+def existsObj(id: str) -> bool:
+    # Use static memory
+    return IPCMemory.exists(id)
 
 def init():
     # Check if uptime is less than 10 seconds
