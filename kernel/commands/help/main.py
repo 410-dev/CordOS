1d1b7c590201d43d1a6fb4b1e4b75f121125616b
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-04-06 21:51:07 +0900

    Converted to method-only style

--- kernel/commands/help/main.py
+++ kernel/commands/help/main.py
@@ -6,39 +6,32 @@ import kernel.registry as Registry
 import json
 import os
 
+async def main(args: list, message) -> None:
+    try:
+        commandPaths: List[str] = json.loads(Registry.read("SOFTWARE.CordOS.Kernel.Programs.Paths"))['data']
 
+        if len(args) < 2:
+            # Print current manual
+            with open("kernel/commands/help/manual.txt", 'r') as f:
+                await message.reply(f.read(), mention_author=True)
+                return
 
-class Help:
-    
-    def __init__(self, lineArgs, message) -> None:
-        self.args: list = lineArgs
-        self.message = message
+        # Find executable bundle
+        helpString: str = ""
+        for commandPath in commandPaths:
+            try:
+                with open(os.path.join(commandPath, args[1], "manual.txt"), 'r') as f:
+                    helpString = f.read()
+                    break
+            except:
+                pass
 
-    async def exec(self):
-        try: 
-            commandPaths: List[str] = json.loads(Registry.read("SOFTWARE.CordOS.Kernel.Programs.Paths"))['data']
-            
-            if len(self.args) < 2:
-                # Print current manual
-                with open("kernel/commands/help/manual.txt", 'r') as f:
-                    await self.message.reply(f.read(), mention_author=True)
-                    return
-            
-            # Find executable bundle
-            helpString: str = ""
-            for commandPath in commandPaths:
-                try:
-                    with open(os.path.join(commandPath, self.args[1], "manual.txt"), 'r') as f:
-                        helpString = f.read()
-                        break
-                except:
-                    pass
-            
-            # If not found, return command not found.
-            if helpString == "":
-                return Registry.read("SOFTWARE.CordOS.Kernel.Proc.CommandNotFound")
-            
-            await self.message.reply(helpString, mention_author=True)
-        except Exception as e:
-            if Registry.read("SOFTWARE.CordOS.Kernel.PrintTraceback") == "1": traceback.print_exc()
-            await self.message.reply(f"Error in settings. e: {e}", mention_author=True)
+        # If not found, return command not found.
+        if helpString == "":
+            return Registry.read("SOFTWARE.CordOS.Kernel.Proc.CommandNotFound")
+
+        await message.reply(helpString, mention_author=True)
+    except Exception as e:
+        if Registry.read("SOFTWARE.CordOS.Kernel.PrintTraceback") == "1": traceback.print_exc()
+        await message.reply(f"Error in settings. e: {e}", mention_author=True)
+
