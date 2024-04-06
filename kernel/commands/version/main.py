d63b0067b03ffbcae8545531a3a029c32ac316b5
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-04-06 18:35:46 +0900

    command version: Added build

--- kernel/commands/version/main.py
+++ kernel/commands/version/main.py
@@ -12,9 +12,10 @@ class Version:
         try: 
             foundation = Registry.read("SOFTWARE.CordOS.Kernel.Profiles.Foundation")
             version = Registry.read("SOFTWARE.CordOS.Kernel.Profiles.Version")
+            build = Registry.read("SOFTWARE.CordOS.Kernel.Profiles.Build")
             botName = Registry.read("SOFTWARE.CordOS.Kernel.Profiles.BotName")
             botVer = Registry.read("SOFTWARE.CordOS.Kernel.Profiles.BotVersion")
             isSafe = "" if not IPC.read("kernel.safemode") else " (Safe Mode)"
-            await self.message.reply(f"Version Profiling:\n\nBaseSystem: {foundation} {version} {isSafe}\nBot: {botName} {botVer}", mention_author=True)
+            await self.message.reply(f"Version Profiling:\n\nBaseSystem: {foundation} {version} (build {build}) {isSafe}\nBot: {botName} {botVer}", mention_author=True)
         except Exception as e:
             await self.message.reply(f"Error loading profile: {e}", mention_author=True)
