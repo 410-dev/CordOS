682fd0416d7e4e7151b641497a568f0e65c66064
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-04-06 21:46:06 +0900

    launchcmd: Now supports both class structure and method-only structure

--- kernel/launchcmd.py
+++ kernel/launchcmd.py
@@ -46,6 +46,18 @@ async def runRunnableModule(module: str, args: list, message):
     if Registry.read("SOFTWARE.CordOS.Kernel.ReloadOnCall") == "1":
         importlib.reload(module)
 
-    commandClass = getattr(module, args[0].capitalize())
-    command = commandClass(args, message)
-    await command.exec()
+    # Try with class structure
+    try:
+        commandClass = getattr(module, args[0].capitalize())
+        command = commandClass(args, message)
+        await command.exec()
+
+    except Exception as ignored:
+        # Try with function structure
+        # Call the function where signature is async def main(lineArgs: list, message) -> None:
+        try:
+            await module.main(args, message)
+        except Exception as e:
+            await message.reply(f"Error while running executive. e: {e}", mention_author=True)
+            return
+
