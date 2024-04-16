db1062ac136b507a9b0e4939801a5cbbd3812703
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-04-17 00:50:07 +0900

    install: updated to function structure

--- kernel/commands/install/main.py
+++ kernel/commands/install/main.py
@@ -3,30 +3,25 @@ import kernel.config as Config
 import kernel.registry as Registry
 import kernel.servers as Servers
 
-class Install:
-
-    def __init__(self, lineArgs, message) -> None:
-        self.args: list = lineArgs
-        self.message = message
-        self.config = Config.load()
-        self.regPermission = Registry.read("SOFTWARE.CordOS.Security.Install")
-        self.user = Servers.getUserAtServer(self.message.guild.id, self.message.author.id)
-
-    async def chkPermission(self, permission):
-        if not self.user.hasPermission(permission):
-            await self.message.reply(f"You do not have permission to use this command. (Requires {permission})", mention_author=True)
-            return False
-        return True
-
-    async def exec(self):
-        try:
-            # Check if user has permission to use this command
-            if not await self.chkPermission(self.regPermission): return
+from objects.discordmessage import DiscordMessageWrapper
 
 
-            await self.message.reply("This command is not implemented yet.", mention_author=True)
+async def chkPermission(message: DiscordMessageWrapper):
+    if not Servers.getUserAtServer(message.getMessageObject().guild.id, message.getMessageObject().author.id).hasPermission(Registry.read("SOFTWARE.CordOS.Security.Install")):
+        await message.reply(f"You do not have permission to use this command. (Requires {Registry.read("SOFTWARE.CordOS.Security.Install")})", mention_author=True)
+        return False
+    return True
 
-        except Exception as e:
-            if Registry.read("SOFTWARE.CordOS.Kernel.PrintTraceback") == "1": traceback.print_exc()
-            await self.message.reply(f"Error in settings. e: {e}", mention_author=True)
+
+async def main(args, message: DiscordMessageWrapper):
+    try:
+        # Check if user has permission to use this command
+        if not await chkPermission(message):
+            return
+
+        await message.reply("This command is not implemented yet.", mention_author=True)
+
+    except Exception as e:
+        if Registry.read("SOFTWARE.CordOS.Kernel.PrintTraceback") == "1": traceback.print_exc()
+        await message.reply(f"Error while running install command. e: {e}", mention_author=True)
 
