6cfddeb56feba74b11244bbc08abe429fb34ff23
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-04-26 03:50:43 +0900

    fasthelp now supports SystemUI (sync) mode

--- kernel/commands/fasthelp/main.py
+++ kernel/commands/fasthelp/main.py
@@ -17,9 +17,13 @@ async def mainAsync(args: list, message) -> None:
                 dirList = os.listdir(commandPath)
                 # For each directory in the command path, check if it is a directory and contains main.py.
                 for directory in dirList:
-                    if os.path.isdir(os.path.join(commandPath, directory)) and os.path.exists(
-                            os.path.join(commandPath, directory, "main.py")):
-                        cmdList += f"{directory}\n"
+                    if os.path.isdir(os.path.join(commandPath, directory)) and os.path.exists(os.path.join(commandPath, directory, "main.py")):
+                        with open(os.path.join(commandPath, directory, "main.py"), 'r') as f:
+                            content = f.read()
+                            if "async def mainAsync(" in content:
+                                cmdList += f"{directory}\n"
+                            else:
+                                cmdList += f"{directory} (incompatible)\n"
             except:
                 pass
 
@@ -29,3 +33,30 @@ async def mainAsync(args: list, message) -> None:
     except Exception as e:
         if Registry.read("SOFTWARE.CordOS.Kernel.PrintTraceback") == "1": traceback.print_exc()
         await message.reply(f"Error in settings. e: {e}", mention_author=True)
+
+
+import kernel.io as IO
+
+def main(args: list) -> None:
+    try:
+        commandPaths: List[str] = Registry.read("SOFTWARE.CordOS.Kernel.Programs.Paths").replace(", ", ",").split(",")
+        cmdList: str = "Available Commands:\n"
+        for commandPath in commandPaths:
+            try:
+                dirList = os.listdir(commandPath)
+                for directory in dirList:
+                    if os.path.isdir(os.path.join(commandPath, directory)) and os.path.exists(os.path.join(commandPath, directory, "main.py")):
+                        with open(os.path.join(commandPath, directory, "main.py"), 'r') as f:
+                            content = f.read()
+                            if "def main(" in content:
+                                cmdList += f"{directory}\n"
+                            else:
+                                cmdList += f"{directory} (incompatible)\n"
+            except:
+                pass
+
+        IO.printf(cmdList)
+    except Exception as e:
+        if Registry.read("SOFTWARE.CordOS.Kernel.PrintTraceback") == "1": traceback.print_exc()
+        IO.printf(f"Error in settings. e: {e}")
+
