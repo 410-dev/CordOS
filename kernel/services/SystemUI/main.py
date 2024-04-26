e5f57251a85fc8bc4e32cc460e475a1855b8e876
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-04-26 13:27:40 +0900

    bug fix

--- kernel/services/SystemUI/main.py
+++ kernel/services/SystemUI/main.py
@@ -9,21 +9,25 @@ import kernel.io as IO
 def main():
     IO.println("====SystemUI Shell Mode====")
 
-    while not IPC.read("kernel.off"):
+    while not IPC.read("power.off"):
         runnablePath = ""
         cmd = ""
         args: list = []
 
         try:
             msgContent: str = input("> ")
+            if msgContent == "":
+                continue
             args: list = Launcher.splitArguments(msgContent)
             cmd: str = Launcher.getCommand(args)
             runnablePath: str = Launcher.getRunnableModule(args)
         except Exception as e:
             IO.println(f"Failed looking up for command. This should not occur. {e}")
+            continue
 
         if runnablePath == "" or runnablePath is None:
             IO.println(f"Command {cmd} not found.")
+            continue
 
         try:
             Launcher.launchRunnable(runnablePath, args)
@@ -31,3 +35,4 @@ def main():
             if Registry.read("SOFTWARE.CordOS.Kernel.PrintErrors") == "1": IO.println(f"Error executing command '{cmd}': {e}")
             if Registry.read("SOFTWARE.CordOS.Kernel.PrintTraceback") == "1": traceback.print_exc()
             IO.println(f"Error executing command '{cmd}': {e}")
+
--- system.py
+++ system.py
@@ -1,4 +1,3 @@
-import single
 
 try:
     import json
