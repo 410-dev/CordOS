262a7cb7a3952af9a3e27804f5f3cc9fbb8f9ad7
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-04-26 21:54:05 +0900

    SafeShutdownTracker.configure: Enable disable added

--- kernel/services/SafeShutdownTracker/configure.py
+++ kernel/services/SafeShutdownTracker/configure.py
@@ -1,2 +1,33 @@
+import kernel.registry as Registry
+import kernel.io as IO
+
 async def mainAsync(args: list, message):
-    await message.reply("This service does not support configuration.")
+    if len(args) < 1:
+        await message.reply("Usage: SafeShutdownTracker <enabled|disabled>")
+        return
+
+    if args[0] == "enabled":
+        Registry.write("SOFTWARE.CordOS.Kernel.Services.kernel.services.SafeShutdownTracker.Enabled", "1")
+        await message.reply("SafeShutdownTracker enabled.")
+    elif args[0] == "disabled":
+        Registry.write("SOFTWARE.CordOS.Kernel.Services.kernel.services.SafeShutdownTracker.Enabled", "0")
+        await message.reply("SafeShutdownTracker disabled.")
+    else:
+        await message.reply("Invalid argument. Expected enabled or disabled.")
+
+
+def main(args: list):
+    if len(args) < 1:
+        IO.println("Usage: SafeShutdownTracker <enabled|disabled>")
+        return
+
+    if args[0] == "enabled":
+        Registry.write("SOFTWARE.CordOS.Kernel.Services.kernel.services.SafeShutdownTracker.Enabled", "1")
+        IO.println("SafeShutdownTracker enabled.")
+    elif args[0] == "disabled":
+        Registry.write("SOFTWARE.CordOS.Kernel.Services.kernel.services.SafeShutdownTracker.Enabled", "0")
+        IO.println("SafeShutdownTracker disabled.")
+    else:
+        IO.println("Invalid argument. Expected enabled or disabled.")
+
+
