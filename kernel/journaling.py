719e4d9ef9cf23d1bad684cb877b08484563971c
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-04-26 23:15:47 +0900

    kernel/journaling.py: Updated

--- kernel/journaling.py
+++ kernel/journaling.py
@@ -1,6 +1,43 @@
 import datetime
 import traceback
-def addJournal(text: str):
+import os
+
+import kernel.clock as Clock
+import kernel.partitionmgr as PartitionMgr
+
+def record(state: str, text: str):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     caller = traceback.extract_stack(None, 2)[0]
-    print(f"[{timestamp}] {caller.filename} - {text}")
+
+    callerName = caller.name
+    if "<" in callerName or "main" == callerName or "mainAsync" in callerName:
+        bundleName = os.path.dirname(caller.filename).replace("//", "/").replace("\\", "/").split("/")[-1]
+        scriptFileName = os.path.basename(caller.filename).replace(".py", "")
+        callerName = f"{bundleName}.{scriptFileName}"
+
+
+    print(f"[{timestamp}] [{state}] {caller.name}@{callerName}: {text}")
+
+    specificJournal = f"{PartitionMgr.data()}/etc/journals/{Clock.getStartTime().split(".")[0]}/{callerName}.journal"
+    globalJournal = f"{PartitionMgr.data()}/etc/journals/{Clock.getStartTime().split(".")[0]}/_.journal"
+
+    # Create directory
+    directory = os.path.dirname(specificJournal)
+    os.makedirs(directory, exist_ok=True)
+
+    # Write to file if not exists
+    if not os.path.exists(specificJournal):
+        with open(specificJournal, "w") as f:
+            scriptPath = caller.filename.replace("//", "/").replace("\\", "/").replace(os.path.abspath(PartitionMgr.root()).replace("//", "/").replace("\\", "/"), "")
+            f.write(f"Script Location: {scriptPath}\n")
+            f.write(f"[{timestamp}] [{state}] [{caller.name}@{callerName}] {text}\n")
+    else:
+        with open(specificJournal, "a") as f:
+            f.write(f"[{timestamp}] [{state}] [{caller.name}@{callerName}] {text}\n")
+
+    if not os.path.exists(globalJournal):
+        with open(globalJournal, "w") as f:
+            f.write(f"[{timestamp}] [{state}] [{caller.name}@{callerName}] {text}\n")
+    else:
+        with open(globalJournal, "a") as f:
+            f.write(f"[{timestamp}] [{state}] [{caller.name}@{callerName}] {text}\n")
