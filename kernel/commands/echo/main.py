b42b2c8e1c7e4318aa2251393f9123bc116d5cb5
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-05-05 01:17:51 +0900

    new echo command

---
+++ kernel/commands/echo/main.py
@@ -0,0 +1,13 @@
+import kernel.registry as Registry
+import kernel.io as IO
+
+import traceback
+
+def main(args: list) -> None:
+    try:
+        args.pop(0)
+        IO.println(" ".join(args))
+    except Exception as e:
+        if Registry.read("SOFTWARE.CordOS.Kernel.PrintTraceback") == "1": traceback.print_exc()
+        IO.println(f"Error while executing command: {e}")
+
