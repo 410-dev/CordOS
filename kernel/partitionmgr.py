183b898546c04e7d7eb33d098d5bdc91c3e217ac
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-05-08 09:47:59 +0900

    Update partitionmgr.py

--- kernel/partitionmgr.py
+++ kernel/partitionmgr.py
@@ -1,12 +1,44 @@
+import os
+import shutil
+
 
 def data() -> str:
-    return "data"
+    return "storage"
+
 
 def cache() -> str:
     return f"{data()}/cache"
 
+
 def files() -> str:
     return f"{data()}/files"
 
+
 def root() -> str:
     return f"./"
+
+
+def write(path: str, content: str):
+    with open(os.path.join(data(), path), "w") as f:
+        f.write(content)
+
+
+def read(path: str) -> str:
+    with open(os.path.join(data(), path), "r") as f:
+        return f.read()
+
+
+def isFile(path: str) -> bool:
+    return os.path.isfile(os.path.join(data(), path))
+
+
+def isDir(path: str) -> bool:
+    return os.path.isdir(os.path.join(data(), path))
+
+
+def exists(path: str) -> bool:
+    return os.path.exists(os.path.join(data(), path))
+
+
+def mkdir(path: str):
+    os.makedirs(os.path.join(data(), path), exist_ok=True)
