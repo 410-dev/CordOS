401137980249db4c2ffb495ecdc3ef9ce0cff268
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-05-12 19:30:55 +0900

    StandardIODevice fix

--- kernel/drivers/StandardIODevice.py
+++ kernel/drivers/StandardIODevice.py
@@ -2,5 +2,5 @@ def stdio_input(prompt: str) -> str:
     return input(prompt)
 
 
-def stdio_output(string: str, end: str = '\n', flush: bool = False):
+def stdio_output(string: str, end: str = '', flush: bool = False):
     print(string, end=end, flush=flush)
