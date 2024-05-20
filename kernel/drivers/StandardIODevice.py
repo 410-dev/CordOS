fe49e304428286b8028465c85dc02cad79c6da7d
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-05-21 00:09:53 +0900

    StandardIODevice supports class format

--- kernel/drivers/StandardIODevice.py
+++ kernel/drivers/StandardIODevice.py
@@ -1,3 +1,11 @@
+class StandardIODevice:
+    def stdio_input(prompt: str) -> str:
+        return input(prompt)
+
+    def stdio_output(string: str, end: str = '', flush: bool = False):
+        print(string, end=end, flush=flush)
+
+
 def stdio_input(prompt: str) -> str:
     return input(prompt)
 
