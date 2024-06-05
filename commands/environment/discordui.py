63d50372f36487109c69a52a58edd06d1d54f9ed
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-06-05 11:12:08 +0900

    Implemented unimplemented codes

--- commands/environment/discordui.py
+++ commands/environment/discordui.py
@@ -1,2 +1,8 @@
 # Shows the current environment is either isolation or global
+import kernel.services.DiscordUIService.subsystem.sv_isolation as Isolation
+
 async def mainAsync(args: list, message):
+    if Isolation.getIsolationAvailable(message):
+        await message.reply("Environment: Isolation", mention_author=True)
+    else:
+        await message.reply("Environment: Global", mention_author=True)
--- commands/multiline-execute/discordui.py
+++ commands/multiline-execute/discordui.py
@@ -1,1 +1,16 @@
-async def mainAsync(args, message):
+import kernel.events.interaction.DiscordUIInterpreter.discordui as DiscordUIInterpreter
+
+import kernel.registry as Registry
+
+from kernel.services.DiscordUIService.objects.discordmessage import DiscordMessageWrapper
+
+async def mainAsync(args, message: DiscordMessageWrapper):
+    prefix = Registry.read("SOFTWARE.CordOS.Config.Core.Prefix")
+    msgContent: str = message.content[len(prefix):]
+    args = msgContent.split("\n")
+    if args[0] != "multiline-execute":
+        await message.reply("This command should not contain other command in caller execution.", mention_author=True)
+        return
+    args.pop(0)
+    for arg in args:
+        await DiscordUIInterpreter.execute(arg, message)
