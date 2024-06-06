d9e6ba9ba38a3ce895978722dbd53ecda6868dbc
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-06-07 00:56:23 +0900

    Update profile.py

--- kernel/profile.py
+++ kernel/profile.py
@@ -8,9 +8,13 @@ def isPackageSDKCompatible(sdkv: int):
 
 
 def isServiceSDKCompatible(sdkv: int):
-    # return Registry.read("SOFTWARE.CordOS.Kernel.Services.Compatibility.SDKMinimum") <= sdkv <= Registry.read("SOFTWARE.CordOS.Kernel.Services.Compatibility.SDKMaximum")
-    return int(Registry.read("SOFTWARE.CordOS.Kernel.Services.Compatibility.SDKMinimum", default=0)) <= sdkv <= int(Registry.read("SOFTWARE.CordOS.Kernel.Services.Compatibility.SDKMaximum", default=1))
+    sdkRange = getServiceSDKRange()
+    return sdkRange[0] <= sdkv <= sdkRange[1]
 
+
+def getServiceSDKRange():
+    # return Registry.read("SOFTWARE.CordOS.Kernel.Services.Compatibility.SDKMinimum"), Registry.read("SOFTWARE.CordOS.Kernel.Services.Compatibility.SDKMaximum")
+    return int(Registry.read("SOFTWARE.CordOS.Kernel.Services.Compatibility.SDKMinimum", default=0)), int(Registry.read("SOFTWARE.CordOS.Kernel.Services.Compatibility.SDKMaximum", default=1))
 
 def getPackageSDKRange():
     # return Registry.read("SOFTWARE.CordOS.Packages.Compatibility.SDKMinimum"), Registry.read("SOFTWARE.CordOS.Packages.Compatibility.SDKMaximum")
