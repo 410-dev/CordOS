e919dba2aa11e648a5c2171f3d5466c97bd497ba
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2024-06-06 23:47:52 +0900

    Implemented global-sync

--- commands/global-sync/discordui.py
+++ commands/global-sync/discordui.py
@@ -1,3 +1,15 @@
 # Updates the isolated command bundles to the latest version in system global
 
+import kernel.services.DiscordUIService.subsystem.sv_isolation as Isolation
+import kernel.journaling as Journaling
+
 async def mainAsync(args: list, message):
+    if not Isolation.getIsolationPermission(message, "global.commands.execute") or not Isolation.getIsolationPermission(message, "local.commands.modify"):
+        await message.reply(f"Instance does not have permission to update: global.commands.execute: {Isolation.getIsolationPermission(message, 'global.commands.execute')}, local.commands.modify: {Isolation.getIsolationPermission(message, 'local.commands.modify')}", mention_author=True)
+        return
+    await message.reply("Updating isolated command bundles to the latest version in system global.", mention_author=True)
+    Journaling.record("INFO", "Updating isolated command bundles to the latest version in system global.")
+    Isolation.mkIsolation_sync(message, [("commands", "commands")])
+    Journaling.record("INFO", "Updating isolated command bundles to the latest version in system global.")
+    Isolation.mkIsolation_patchImports(message, Isolation.getContainerPath(message, ""))
+    await message.reply("Isolated command bundles updated.", mention_author=True)
